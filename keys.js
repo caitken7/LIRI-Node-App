d172cba37c87fbc51cb08e3f387ca67c344fa97a
Casey Aitken <caseyaitken@gmail.com>
2017-10-16 20:54:16 -0500

    commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<4bjzYHNaKkvWtwOMkR3H3W7wa>',
+  consumer_secret: '<4bjzYHNaKkvWtwOMkR3H3W7wa>',
+  access_token_key: '<4bjzYHNaKkvWtwOMkR3H3W7wa>',
+  access_token_secret: '<4bjzYHNaKkvWtwOMkR3H3W7wa>',
+}
+
+module.exports = twitterKeys;
